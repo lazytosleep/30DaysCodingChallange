2d9a7e9c98345e13686b750bacc0d0e089516ce3
trips <shivendra.tripathi@in.pega.com>
2020-04-04 09:43:49 +0530

    feat:Day 2

--- Day2.py
+++ Day2.py
@@ -4,5 +4,8 @@ def doXOR(nums) -> int:
         xors = xors ^ nums[i]
     return xors
 
-
+'''
+Every element appear twice except once, find the unique element
+XOR will ensure that number appearing twice with cancel out
+'''
 print(doXOR([4, 1, 3, 2, 3, 1, 2]))
