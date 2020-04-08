2c9c3ceacf00b1a737f4c23cde9aa8d8030cd6db
shiv <shivendra.tripathi@in.pega.com>
2020-04-08 23:34:02 +0530

    feat: day 5

---
+++ Day5.py
@@ -0,0 +1,19 @@
+'''
+Maxmize profit by buying and selling multiple times
+'''
+class Solution:
+    def maximizeProfit(self, arr, start) -> int:
+        maxSoFor = 0
+        if start == len(arr):
+            return maxSoFor
+        for i in range(start, len(arr)-1):
+            for j in range(i+1, len(arr)):
+                if arr[j] > arr[i]:
+                    max = arr[j] - arr[i] + self.maximizeProfit(arr, j+1)
+                    if max > maxSoFor:
+                        maxSoFor = max
+        return maxSoFor
+
+
+print(Solution().maximizeProfit([7,1,5,3,6,4], 0))
+
