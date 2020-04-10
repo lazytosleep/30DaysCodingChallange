1ef706cd37c1259e0c00d55231bb90881dfb0a51
shiv <shivendra.tripathi@in.pega.com>
2020-04-10 14:46:32 +0530

    feat: Day 7

--- Day6.py
+++ Day6.py
@@ -6,7 +6,7 @@ Group anagram
 def groupAnagram(arr):
     # Create a dictionary with sorted str and their index
     # Iterate over dictionary and get origianl elements at each index
-    # So fundamentaly we are sorting to do the grouping and then capturing the index
+    # So fundamentaly we are sorting to do the grouping the anagrams in a bucket and then caollecting their index
     dict = {}
     for i in range(0, len(arr)):
         sortedStr = ''.join(sorted(arr[i]))
